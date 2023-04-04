1e673ff0a132e4e6b4dbabc96b7cdf64f6e363d2
satriaherman1 <muhammadsatriaherman@gmail.com>
2023-04-04 20:26:58 +0700

    add gsap navbar

--- src/components/containers/home/Header.tsx
+++ src/components/containers/home/Header.tsx
@@ -1,31 +1,31 @@
-import { Box, BoxProps, Button, Container, Flex, Heading, Icon, Image, Text, useMediaQuery } from "@chakra-ui/react";
+import { Box, BoxProps, Button, Container, Flex, Heading, Icon, Image, ImageProps, Text, useMediaQuery } from "@chakra-ui/react";
 import { containerMaxWidth, largeBreakpoints, mediumBreakpoints } from "@src/definitions/variables";
-import { Expo, gsap } from "gsap";
-import { HiOutlineChevronLeft, HiOutlineChevronRight } from "react-icons/hi";
+import { Expo, TweenMax, gsap } from "gsap";
 import { useEffect, useRef } from "react";
-
-import { Link } from "react-router-dom";
+import { Tween } from "react-gsap";
 
 export default function HeaderHome(props: BoxProps) {
   const [mediumScreen] = useMediaQuery(mediumBreakpoints);
   const [largeScreen] = useMediaQuery(largeBreakpoints);
 
   const imageRef = useRef(null);
-  const boxRef1 = useRef(null);
-  const boxRef2 = useRef(null);
-  const chevronRef1 = useRef(null);
   const ctaButton = useRef(null);
+  const boxImageRef = useRef(null);
+
+  const statistic1Ref = useRef(null);
 
   useEffect(() => {
-    // gsap.fromTo(boxRef1.current, { opacity: 0, y: -800, ease: Expo.easeIn }, { duration: 1, opacity: 1, y: 0, ease: Expo.easeInOut });
-    // gsap.fromTo(boxRef2.current, { opacity: 0, y: -800, ease: Expo.easeInOut }, { delay: 0.5, duration: 1, opacity: 1, y: 0, ease: Expo.easeInOut });
-    gsap.fromTo(imageRef.current, { opacity: 0, y: -400, ease: Expo.easeInOut }, { duration: 2, opacity: 1, y: 0, ease: Expo.easeInOut });
-
-    gsap.fromTo(chevronRef1.current, { opacity: 0, y: -20, ease: Expo.easeInOut }, { duration: 1, opacity: 1, y: 0, repeat: -1, yoyo: true, ease: Expo.easeInOut });
+    gsap.fromTo(boxImageRef.current, { opacity: 0, width: 0, ease: Expo.easeIn }, { duration: 2, opacity: 1, width: "100%", ease: Expo.easeInOut });
+    gsap.fromTo(imageRef.current, { opacity: 0, scale: 0.4, ease: Expo.easeInOut }, { duration: 2, opacity: 1, scale: 1, ease: Expo.easeInOut });
+    TweenMax.to(statistic1Ref.current, 2, {
+      value: 500,
+      // Animasi properti 'ease' dari lambat ke cepat
+      ease: "power4.out",
+    });
   }, []);
 
   const clickAnimation = () => {
-    gsap.to(ctaButton.current, {
+    gsap.to(ctaButton.current, 2, {
       scale: 0.8,
       opacity: 0.8,
       duration: 0.2,
@@ -41,8 +41,16 @@ export default function HeaderHome(props: BoxProps) {
     });
   };
 
+  const imgProperty: ImageProps = {
+    loading: "lazy",
+    display: "block",
+    mx: "auto",
+
+    fallbackSrc: "https://res.cloudinary.com/dbg3ckwtz/image/upload/v1677231020/investment-competition/fallback_qvvcgt.svg",
+  };
+
   return (
-    <Box {...props} as="header">
+    <Box {...props} as="header" marginTop="140px">
       <Container
         position="relative"
         maxW={containerMaxWidth}
@@ -53,39 +61,48 @@ export default function HeaderHome(props: BoxProps) {
         rowGap="40px"
       >
         <Box display="flex" alignItems="center" flexBasis={mediumScreen ? "48%" : "100%"}>
-          <Box>
-            <Heading size="2xl">
-              Temukan Modal Usaha untuk Bisnis{" "}
-              <Box as="span" color="teal.500">
-                UMKM
-              </Box>{" "}
-              Anda
-            </Heading>
+          <Box marginTop="20px">
+            <Heading size="2xl">Lestarikan Budaya Daerahmu dan Kenalkan Kepada Dunia Anda</Heading>
             <Text marginTop="25px" textTransform="capitalize">
-              kini mencari modal usaha semudah membuka ponsel hp anda Bersama InvestKU. Cukup dengan mengisikan data UMKM anda dan tunggu verifikasi dari sistem kami untuk mendapatkan dana!
+              Gabung bersama kami di komunitas budaya untuk melestarikan Budaya daerahmu dan membawanya ke kancah dunia!
             </Text>
 
             <Button className="start-now" marginTop="25px" size="lg" variant="solid" colorScheme="teal" ref={ctaButton} onClick={clickAnimation}>
               Daftar Sekarang!
             </Button>
+
+            <Flex marginTop="50px" gap="40px">
+              <Box position="relative">
+                <Tween ref={statistic1Ref} from={{ value: 0 }}>
+                  <Heading>{0}</Heading>
+                </Tween>
+
+                <Text>Anggota</Text>
+                <Image src="https://res.cloudinary.com/dbg3ckwtz/image/upload/v1680610705/Indoture/decorator_lup2rj.png" position="absolute" top="-8px" left="-8px" w="40px" zIndex="-1" />
+              </Box>
+              <Box position="relative">
+                <Heading>20</Heading>
+                <Text>Prestasi</Text>
+                <Image src="https://res.cloudinary.com/dbg3ckwtz/image/upload/v1680610161/Indoture/statistic-decorator2_o3rfzx.png" position="absolute" top="-8px" left="-8px" w="40px" zIndex="-1" />
+              </Box>
+              <Box position="relative">
+                <Heading>10 </Heading>
+                <Text>Tahun Berdiri</Text>
+                <Image src="https://res.cloudinary.com/dbg3ckwtz/image/upload/v1680610273/Indoture/decorator_smiipk.png" position="absolute" top="-8px" left="-8px" w="40px" zIndex="-1" />
+              </Box>
+            </Flex>
           </Box>
         </Box>
 
-        <Flex justifyContent="space-between" flexBasis={mediumScreen ? "48%" : "100%"}>
-          {/* <Box ref={boxRef1} w={mediumScreen ? "3vw" : "3vw"} h="180vh" bg="green.500" position="absolute" top="-80vh" right="24vw" zIndex="-1"></Box>
-          <Box ref={boxRef2} w={mediumScreen ? "3vw" : "3vw"} h="180vh" bg="gray.300" position="absolute" top="-80vh" right="28vw" zIndex="-1"></Box> */}
-          <Image
-            ref={imageRef}
-            src="https://res.cloudinary.com/dbg3ckwtz/image/upload/v1680497518/Indoture/header_ljhsxn.png"
-            loading="lazy"
-            display="block"
-            mx="auto"
-            maxH="500px"
-            fallbackSrc="https://res.cloudinary.com/dbg3ckwtz/image/upload/v1677231020/investment-competition/fallback_qvvcgt.svg"
-          />
+        <Flex gap="45px" justifyContent="space-between" flexBasis={mediumScreen ? "48%" : "100%"} position="relative">
+          {/* <Box ref={boxRef1} w={mediumScreen ? "3vw" : "3vw"} h="180vh" bg="green.500" position="absolute" top="-80vh" right="24vw" zIndex="-1"></Box> */}
+          <Box ref={boxImageRef} w={mediumScreen ? "40vw" : "32vw"} h={mediumScreen ? "170vh" : "150vh"} bg="teal.600" position="absolute" top="-80vh" right="-10vw" zIndex="-1"></Box>
+          <Flex flexWrap="wrap">
+            <Image ref={imageRef} src="https://res.cloudinary.com/dbg3ckwtz/image/upload/v1680608404/Indoture/header1_htnexi.png" {...imgProperty} />
+          </Flex>
         </Flex>
 
-        <Box ref={chevronRef1} position="absolute" bottom="-20%" right="50%" display="flex" gap="9px" alignItems="center" height="fit-content" transform="rotate(90deg)">
+        {/* <Box ref={chevronRef1} position="absolute" bottom="-20%" right="50%" display="flex" gap="9px" alignItems="center" height="fit-content" transform="rotate(90deg)">
           Scroll{" "}
           <Box height="fit-content" display="flex" alignItems="center">
             <Box as="span" display="flex" alignItems="center">
@@ -98,7 +115,7 @@ export default function HeaderHome(props: BoxProps) {
               <Icon as={HiOutlineChevronRight} w="12px" />
             </Box>
           </Box>
-        </Box>
+        </Box> */}
       </Container>
     </Box>
   );
--- src/components/fragments/Navbar/index.tsx
+++ src/components/fragments/Navbar/index.tsx
@@ -3,22 +3,26 @@ import { Box, Button, Container, Divider, Flex, Heading, Icon, List, ListItem, u
 // import Switch from "@src/components/common/Switch";
 import { navigationList } from "@src/components/fragments/Navbar/data";
 import { boxShadowColor, containerMaxWidth, mediumBreakpoints } from "@src/definitions/variables";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./style.css";
 
 import { Link } from "react-router-dom";
+import { gsap } from "gsap";
 
 export default function Navbar() {
   const [openNav, setOpenNav] = useState<boolean>(false);
-  const [isBg, setIsBg] = useState<boolean>(false);
+  const [isBg, setIsBg] = useState<boolean>(true);
 
   const [mediumScreen] = useMediaQuery(mediumBreakpoints);
   const bg = useColorModeValue("white", "#282e39");
   const linkActiveColor = useColorModeValue("teal.500", "teal.300");
   const boxShadow = useColorModeValue(boxShadowColor, "none");
 
+  const smallNavRef = useRef(null);
+  const smallNavBgRef = useRef(null);
+
   const setBgNavbar = () => {
-    window.scrollY > 20 ? setIsBg(true) : setIsBg(false);
+    window.scrollY > 20 ? setIsBg(true) : setIsBg(true);
   };
 
   useEffect(() => {
@@ -26,13 +30,108 @@ export default function Navbar() {
     return () => window.removeEventListener("scroll", setBgNavbar);
   });
 
+  useEffect(() => {
+    if (openNav) {
+      gsap.fromTo(smallNavBgRef.current, { opacity: 0 }, { borderRadius: 0, opacity: 1, width: "100vw", height: "90vh", top: "10vh" });
+      gsap.fromTo(
+        smallNavRef.current,
+        { width: 0, height: 0 },
+        {
+          width: "100vw",
+          height: "90vh",
+        },
+      );
+      gsap.fromTo(
+        ".nav-list-small-gsap-active",
+        {
+          opacity: 0,
+          marginTop: "20px",
+        },
+        {
+          duration: 0.3,
+          opacity: 1,
+          marginTop: 0,
+        },
+      );
+      gsap.fromTo(
+        ".nav-list-small-gsap-0",
+        {
+          opacity: 0,
+          marginTop: "20px",
+        },
+        {
+          duration: 0.3,
+          opacity: 1,
+          marginTop: 0,
+        },
+      );
+      gsap.fromTo(
+        ".nav-list-small-gsap-1",
+        {
+          opacity: 0,
+          marginTop: "20px",
+        },
+        {
+          duration: 0.3,
+          opacity: 1,
+          marginTop: 0,
+        },
+      );
+      gsap.fromTo(
+        ".nav-list-small-gsap-2",
+        {
+          opacity: 0,
+          marginTop: "20px",
+        },
+        {
+          duration: 0.3,
+          opacity: 1,
+          marginTop: 0,
+        },
+      );
+    } else {
+      gsap.to(smallNavRef.current, {
+        delay: 0.2,
+        width: 0,
+        height: 0,
+      });
+      gsap.to(smallNavBgRef.current, {
+        delay: 0.2,
+        opacity: 0,
+        height: 0,
+        top: 0,
+      });
+
+      // nav list
+      gsap.to(".nav-list-small-gsap-active", 0.2, {
+        duration: 0.2,
+        opacity: 0,
+        marginTop: "20px",
+      });
+      gsap.to(".nav-list-small-gsap-0", 0.2, {
+        opacity: 0,
+        marginTop: "20px",
+      });
+
+      gsap.to(".nav-list-small-gsap-1", 0.2, {
+        opacity: 0,
+        marginTop: "20px",
+      });
+
+      gsap.to(".nav-list-small-gsap-2", 0.2, {
+        opacity: 0,
+        marginTop: "20px",
+      });
+    }
+  }, [openNav]);
+
   return (
-    <Box as="nav" paddingY="20px" fontWeight={500} className="navbar" bg={isBg ? bg : "unset"} boxShadow={isBg ? `0 0 30px ${boxShadow}` : "unset"} position="fixed" top="0px" w="full" zIndex={998}>
+    <Box as="nav" paddingY="20px" fontWeight={500} className="navbar" bg={isBg ? bg : "unset"} boxShadow={isBg ? `0 0 30px ${boxShadow}` : "unset"} position="fixed" top="0px" w="full" zIndex={999}>
       <Container maxW={containerMaxWidth} display="flex" alignItems="center" justifyContent="space-between">
         <Heading as="h4" size="md">
-          Invest
+          Curtu
           <Box as="span" color="primary.500">
-            KU
+            re
           </Box>
         </Heading>
 
@@ -56,46 +155,36 @@ export default function Navbar() {
           <Button>Login</Button>
         </Box>
 
-        <Box as="button" className="nav-button" display={mediumScreen ? "none" : "flex"} flexDir="column" onClick={() => setOpenNav(true)}>
+        <Box as="button" className="nav-button" display={mediumScreen ? "none" : "flex"} flexDir="column" onClick={() => setOpenNav(!openNav)}>
           <HamburgerIcon fontSize="29px" />
         </Box>
       </Container>
 
       {!mediumScreen && (
-        <Box bg={bg} pos="fixed" top={0} left={openNav ? 0 : "-100vw"} h="100vh" w="100vw" transition="0.3s" zIndex={999}>
-          <Flex justifyContent="space-between">
-            <Heading as="h4" size="md" margin="20px">
-              Invest
-              <Box as="span" color="primary.500">
-                KU
-              </Box>
-            </Heading>
+        <>
+          <Box ref={smallNavBgRef} bg={bg} position="fixed" w="100vw" left="0" zIndex={997}></Box>
+          <Box ref={smallNavRef} bg="none" pos="fixed" height={0} top="8vh" left={0} overflow="hidden" zIndex={997}>
+            <List display="flex" flexDir="column" marginTop="20px">
+              {navigationList.map((nav, key) => (
+                <>
+                  {nav.url == location.pathname ? (
+                    <ListItem className={`nav-list-small nav-list-small-gsap-active`} padding="19px 20px" key={nav.url} color={linkActiveColor}>
+                      <Link to={nav.url}>{nav.name}</Link>
+                    </ListItem>
+                  ) : (
+                    <ListItem className={`nav-list-small nav-list-small-gsap-${key}`} padding="19px 20px" key={nav.url}>
+                      <Link to={nav.url}>{nav.name}</Link>
+                    </ListItem>
+                  )}
+                </>
+              ))}
+            </List>
 
-            <Box as="button" marginRight="22px" onClick={() => setOpenNav(false)}>
-              <CloseIcon />
-            </Box>
-          </Flex>
-          <Divider />
-          <List display="flex" flexDir="column" marginTop="20px">
-            {navigationList.map((nav) => (
-              <>
-                {nav.url == location.pathname ? (
-                  <ListItem className="nav-list-small" padding="19px 20px" key={nav.url} color={linkActiveColor}>
-                    <Link to={nav.url}>{nav.name}</Link>
-                  </ListItem>
-                ) : (
-                  <ListItem className="nav-list-small" padding="19px 20px" key={nav.url}>
-                    <Link to={nav.url}>{nav.name}</Link>
-                  </ListItem>
-                )}
-              </>
-            ))}
-          </List>
-
-          {/* <Box position="absolute" left="22px" bottom="10vh">
+            {/* <Box position="absolute" left="22px" bottom="10vh">
             <Switch />
           </Box> */}
-        </Box>
+          </Box>
+        </>
       )}
     </Box>
   );
